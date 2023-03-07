ae799b6e9e981403c3352259496a7254a6f913ef
John Dellape <dellapejw@gmail.com>
2023-03-07 16:18:37 -0500

    add descriptions to home page

--- practical-business-streamlit/deduplication/home.py
+++ practical-business-streamlit/deduplication/home.py
@@ -7,9 +7,12 @@ st.set_page_config(
 st.header("Practical Business Streamlit: Data Deduplication")
 st.subheader("Scenarios Explored")
 
-st.markdown("1. Exact Duplication")
-st.markdown("2. Partial Duplication")
-st.markdown("3. Fuzzy Duplication")
+st.markdown("1. **Exact Duplication**: Find rows in which all column values are identical across rows.")
+st.image('images/exact_duplication.png')
+st.markdown("2. **Partial Duplication**: Find rows in which an ID column which we anticipate to be unique is repeated across rows. We know there is a difference in one or more column values causing ID field duplication.")
+st.image('images/partial_duplication.png')
+st.markdown("3. **Fuzzy Duplication**: Find rows where two or more rows are similar enough that they appear to be duplicates. Resolution requires a more technically involved process such as string similarity scoring across multiple columns.")
+st.image('images/fuzzy_duplication.png')
 
 
 
